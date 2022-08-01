66e57234fec8a120496de42624ed75fad016e82f
Vengeful-Pancake <thinh2000tiger@gmail.com>
2022-08-01 23:07:12 +0700

    xxx

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import tcod
- 
+
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
+
 def main() -> None:
     screen_width = 80
     screen_height = 50
@@ -10,7 +14,8 @@ def main() -> None:
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
-    for event in tcod.event.wait():
+
+    event_handler = EventHandler()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -24,9 +29,20 @@ def main() -> None:
             root_console.print(x=player_x, y=player_y, string="@")
 
             context.present(root_console)
+            
+            root_console.clear()
 
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 if __name__ == "__main__":
