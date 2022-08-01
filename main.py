49192c34585bc249bbd79c31fc680f39a4fac34b
Vengeful-Pancake <thinh2000tiger@gmail.com>
2022-08-01 23:01:57 +0700

    new files

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import tcod
-
-class Fighter:
-    def __init__(self, hp, defense, power):
-        self.max_hp = hp
-        self.hp = hp
-        self.defense = defense
-        self.power = power
  
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+    for event in tcod.event.wait():
 
     with tcod.context.new_terminal(
         screen_width,
@@ -24,7 +21,7 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
 
             context.present(root_console)
 
